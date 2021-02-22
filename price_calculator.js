6c4601719ddb45b58113de1a28fd2eaca647a327
Yusef Marra <yusef.marra@sling.com>
2021-02-22 15:34:53 -0700

    Calculate cost and discount

--- price_calculator.js
+++ price_calculator.js
@@ -1,25 +1,42 @@
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
-})
+});
 
-const prices = {
+const PRICES = {
   milk: { price: 3.97, discountQuantity: 2, discountPrice: 5, discount: 2.94 },
-  bread: { price: 2.17, discountQuantity: 3, discountPrice: 6, discount: 0.51 },
+  bread: { price: 2.17, discountQuantity: 2, discountPrice: 6, discount: 0.51 },
   banana: { price: 0.99, discountQuantity: null, discountPrice: null, discount: 0 },
   apple: { price: 0.89, discountQuantity: null, discountPrice: null, discount: 0 },
 }
 
+const calculateDiscount = (item, count) => {
+  const { discount, discountQuantity } = PRICES[item];
+  if (discountQuantity) {
+    return Math.floor(count/discountQuantity) * discount
+  }
+  return 0;
+}
+
+const calculateCost = (countedItems) => {
+  return Object.entries(countedItems).map(([item, count]) => {
+    const discount = calculateDiscount(item, count);
+    return { item, count, cost: (PRICES[item].price * count) - discount, saved: discount };
+  })
+}
+
 readline.question('Please enter all the items purchased separated by a comma: ', list => {
   const items = list.split(',').map(item => item.trim());
-  console.log(items);
+  // console.log(items);
 
   const countedItems = items.reduce((acc, item) => {
     acc[item] ? acc[item]++ : acc[item] = 1;
     return acc;
   }, {})
-  console.log(countedItems);
+  // console.log(countedItems);
 
+  const what = calculateCost(countedItems);
+  console.log(what);
 
   readline.close()
-})
+});
