ea9a75187885a22417ab6772541180f7fdac0292
Yusef Marra <yusef.marra@sling.com>
2021-02-22 14:50:36 -0700

    Count items

--- price_calculator.js
+++ price_calculator.js
@@ -3,8 +3,16 @@ const readline = require('readline').createInterface({
   output: process.stdout
 })
 
-readline.question('Please enter all the items purchased separated by a comma ', list => {
-  const items = list.split(',').map((item) => (item.trim()));
-  console.log(list, items);
+readline.question('Please enter all the items purchased separated by a comma: ', list => {
+  const items = list.split(',').map(item => item.trim());
+  console.log(items);
+
+  const countedItems = items.reduce((acc, item) => {
+    acc[item] ? acc[item]++ : acc[item] = 1;
+    return acc;
+  }, {})
+  console.log(countedItems);
+
+
   readline.close()
 })
